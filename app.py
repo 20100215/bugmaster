50984ab448be1939db41032974f59f44b88e2773
20100215 <wdayata@gmail.com>
2025-05-09 16:08:19 +0800

    ref: new code

--- app.py
+++ app.py
@@ -200,7 +200,7 @@ editor_result = None
 if st.session_state.round_started:
     st.markdown("### 🧩 Debug this code")
     editor_result = code_editor(
-        value=st.session_state.code,
+        st.session_state.code,
         height=300,
         language="python",
         theme="light",
